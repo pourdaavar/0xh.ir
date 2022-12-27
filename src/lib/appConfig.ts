27f90f320f170c4680c02b9211ef1e2898d35381
Hossein Pourdavar <h.pourdavar@gmail.com>
2022-12-27 22:44:23 +0330

    seo component added

--- src/lib/appConfig.ts
+++ src/lib/appConfig.ts
@@ -1,9 +1,10 @@
-import { PUBLIC_BASE_URL } from '$env/static/public';
+import { PUBLIC_BASE_URL, PUBLIC_TWITTER_USER } from '$env/static/public';
 
 export const app = {
 	title: '0xh.ir | Daily Tech Stuff',
 	desc: 'description',
 	url: PUBLIC_BASE_URL,
+	twitter: PUBLIC_TWITTER_USER,
 	image:
 		'https://raw.githubusercontent.com/svelte-society/sveltesociety.dev/main/static/images/metatag.png',
 
---
+++ src/lib/components/Seo.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+	import { app } from '$appConfig';
+
+	const brand = app.title;
+
+	export let title: string = brand;
+	export let description = app.desc;
+	export let image = app.image;
+	export let url = app.url;
+	export let twitterHandle = '@realhosssein';
+</script>
+
+<svelte:head>
+	<title>{title ? title : brand}</title>
+	<meta name="description" content={description} />
+
+	<meta property="og:type" content="website" />
+	<meta property="og:title" content={`${title} - ${brand}`} />
+	<meta property="og:site_name" content={brand} />
+	<meta property="og:description" content={description} />
+	<meta property="og:image" name="og:image" content={image} />
+	<meta name="og:url" content={url} />
+
+	<meta name="twitter:site" content={twitterHandle} />
+	<meta name="twitter:image" content={image} />
+	<meta name="twitter:card" content="summary_large_image" />
+	<meta name="twitter:title" content={`${title} - ${brand}`} />
+	<meta name="twitter:description" content={description} />
+</svelte:head>
