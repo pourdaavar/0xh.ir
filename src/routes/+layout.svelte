96b1eb9bee506a587d338e634c3841557fa04f3b
Hossein Pourdavar <h.pourdavar@gmail.com>
2022-12-27 22:45:02 +0330

    Home component

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,14 @@
 <script>
+	import Header from '$components/Header/index.svelte';
 	import '../styles.css';
 </script>
 
-<div id="app">
-	<slot />
+<div
+	id="app"
+	class="scroll-smooth"
+>
+	<div class="flex flex-col h-screen px-6 m-auto text-lg leading-7 bg-neutral text-neutral-900 sm:px-14 md:px-24 lg:px-32 dark:bg-neutral-800 dark:text-neutral max-w-7xl">
+		<Header />
+		<slot />
+	</div>
 </div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,7 @@
+<script>
+  import Seo from "$components/Seo.svelte";
+
+</script>
+
+<Seo />
 <main>Hi</main>
